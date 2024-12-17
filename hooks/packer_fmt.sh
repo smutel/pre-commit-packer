99f2fc24a8831b9c3fa1f112bd3a3360a702498c
smutel <12967891+smutel@users.noreply.github.com>
2024-12-17 10:31:48 +0100

    test

--- hooks/packer_fmt.sh
+++ hooks/packer_fmt.sh
@@ -10,23 +10,23 @@ if [ -z "$(command -v packer)" ]; then
 fi
 
 # The version of readlink on macOS does not support long options
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-readonly SCRIPT_DIR
-# shellcheck source=lib/util.sh
-source "$SCRIPT_DIR/../lib/util.sh"
+# SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+# readonly SCRIPT_DIR
+# # shellcheck source=lib/util.sh
+# source "$SCRIPT_DIR/../lib/util.sh"
 
-util::parse_cmdline "$@"
+# util::parse_cmdline "$@"
 
 error=0
 
-for file in "${FILES[@]}"; do
-  if ! packer fmt "${ARGS[@]}" -- "$file"; then
-    error=1
-    echo
-    echo "Failed path: $file"
-    echo "================================"
-  fi
-done
+# for file in "${FILES[@]}"; do
+  # if ! packer fmt "${ARGS[@]}" -- "$file"; then
+    # error=1
+    # echo
+    # echo "Failed path: $file"
+    # echo "================================"
+  # fi
+# done
 
 if [[ $error -ne 0 ]]; then
   exit 1
